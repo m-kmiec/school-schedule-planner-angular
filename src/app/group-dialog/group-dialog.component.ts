43fc4596fba0043e8f7798f80d895b1f592a28fa
mklubowicz <michal.ghred@gmail.com>
2022-11-26 00:14:36 +0100

    to debug groups

--- src/app/group-dialog/group-dialog.component.ts
+++ src/app/group-dialog/group-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Service } from '../data/data.service';
+import { Course } from '../models/Course';
+
 
 @Component({
   selector: 'app-group-dialog',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-dialog.component.css']
 })
 export class GroupDialogComponent implements OnInit {
+  public groupForm !: FormGroup;
+  public courses: Course[] = [];
+  public courseForm = new FormControl();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+     @Inject(MAT_DIALOG_DATA) public editData: any,
+     private dialogRef: MatDialogRef<GroupDialogComponent>,
+     private service: Service
+     ) {
+      this.service.getCourses().
+      subscribe(data => {
+        this.courses = data;
+        //this.courses = this.courses.filter(e => e.subject.mandatory == false );
+      });
+      }
 
   ngOnInit(): void {
+    this.groupForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      courseForm: ['', Validators.required]
+    })
+
+    if(this.editData){
+      this.groupForm.controls['name'].setValue(this.editData.subject);
+      this.groupForm.controls['courseForm'].setValue(this.editData.teacher);
+    }
+  }
+  addGroup() {
+    if(!this.editData){
+      if (this.groupForm.valid) {
+        this.service.addCourse(this.groupForm.value).
+          subscribe({
+            next: (res) => {
+              alert("Added group!")
+              this.groupForm.reset();
+              this.dialogRef.close('save');
+            },
+            error: () => {
+              alert("Error while adding the group.")
+            }
+          })
+      }
+    }else{
+      this.updateGroup();
+    }
+    
+  }
+  updateGroup(){
+    this.service.editCourse(this.groupForm.value, this.editData.id).
+    subscribe({
+      next: (res) => {
+        alert("Edit group!");
+        this.groupForm.reset();
+        this.dialogRef.close('update');
+      },
+      error: () => {
+        alert("Error while editing the course.")
+      }
+    })
   }
 
 }
--- src/app/models/studentsGroup.ts
+++ src/app/models/studentsGroup.ts
@@ -5,13 +5,13 @@ import { Service } from '../data/data.service';
 export class StudentGroup{
     constructor(
         public name: string,
-        public additionalCourses: Course[],
-        private service: Service
+        public additionalCourses: Course[]
+        // private service: Service
     ){
-        this.service.getCourses().
-        subscribe(data => {
-            this.additionalCourses = data;
-            this.additionalCourses = this.additionalCourses.filter(e => e.subject.mandatory==false)
-        })
+        // this.service.getCourses().
+        // subscribe(data => {
+        //     this.additionalCourses = data;
+        //     this.additionalCourses = this.additionalCourses.filter(e => e.subject.mandatory==true)
+        // })
     }
 }
